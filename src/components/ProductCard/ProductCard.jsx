f1c426d7ba8f45ef145904edc7e1f774f10c83cc
ulianacode <ul1ana.4@yandex.ru>
2025-03-31 15:15:59 +0300

    add all styles

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,26 +1,50 @@
 import React from 'react';
 import './styles.css';
+import caviar from './caviar.jpg';
+import cart from './cart.svg';
+import shrimps from './shrimps.jpg';
+import redfish from './redfish.jpg';
+import keta from './keta.jpg';
+import halibut from './halibut.jpg';
 
 const ProductCard = () => {
     const productList = [
         {
+            id: 1,
             title: "Креветки очищенные",
             cost: "4 200 Ꝑ/кг",
+            pic: shrimps,
         },
         {
-            title: "Креветки очищенные",
-            cost: "4 200 Ꝑ/кг",
+          id: 2,
+          title: "Филе лосося, Норвегия, охлажденный",
+          cost: "2 500 Ꝑ/кг",
+          pic: redfish,
         },
         {
-            title: "Креветки очищенные",
-            cost: "4 200 Ꝑ/кг",
+          id: 3,
+          title: "Красная икра нерки, 250 г/упаковка",
+          cost: "1 200 Ꝑ/кг",
+          pic: caviar,
         },
+        {
+          id: 4,
+          title: "Палочки кеты, 1000 г/упаковка",
+          cost: "2 500 Ꝑ/кг",
+          pic: keta,
+      },
+      {
+        id: 5,
+        title: "Филе палтуса",
+        cost: "2 800 Ꝑ/кг",
+        pic: halibut,
+    },
     ];
     return (
         <div className="section">
         <div className="section__container">
-              <div className="section__buy-with  buy-with">
-                <p className="buy-with__title">С этим товаром покупают</p>
+              <div className="container__suggest  suggest">
+                <p className="suggest__title">С этим товаром покупают</p>
                 <ProductList productList={productList}></ProductList>
         </div>
         </div>
@@ -30,8 +54,8 @@ const ProductCard = () => {
 
 const ProductList = ({ productList }) => {
     return (
-      <div className="buy-with__card"> 
-        <ul className="buy-with__card__list buy-with-card-list">
+      <div className="suggest__card"> 
+        <ul className="suggest__card__list suggest-card-list">
           {productList.map(product => (
             <ProductListItem key={product.id} product={product} />
           ))}
@@ -42,12 +66,14 @@ const ProductList = ({ productList }) => {
 
 const ProductListItem = ({product}) => {
     return (
-        <li className="recommends-card-list__element">
-        <p className="recommends-card-list__element--name">{product.title}</p>
-        <div className="recommends-card-list__cart cart">
+        <li className="suggest-card-list__element">
+        <img width="341" height="256" src={product.pic}/>
+        <p className="suggest-card-list__element--name">{product.title}</p>
+        <div className="suggest-card-list__cart cart">
           <p className="cart__cost">{product.cost}</p>
           <div className="cart_button">
             <a href="#" className="cart_button__cost--link">В корзину</a>
+            <img width="17" height="17" class="cart_button__cost--img" src={cart}/>
           </div>
         </div>
       </li>
