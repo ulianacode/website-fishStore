cec3ad161df5b249b909211418f79bef3c6844f2
ulianacode <ul1ana.4@yandex.ru>
2025-03-31 16:07:20 +0300

    product cards done

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -14,40 +14,54 @@ const ProductCard = () => {
             title: "Креветки очищенные",
             cost: "4 200 Ꝑ/кг",
             pic: shrimps,
+            oldCost: null,
         },
         {
           id: 2,
           title: "Филе лосося, Норвегия, охлажденный",
           cost: "2 500 Ꝑ/кг",
           pic: redfish,
+          oldCost: "5 000 Ꝑ/кг",
         },
         {
           id: 3,
           title: "Красная икра нерки, 250 г/упаковка",
           cost: "1 200 Ꝑ/кг",
           pic: caviar,
+          oldCost: null,
         },
         {
           id: 4,
           title: "Палочки кеты, 1000 г/упаковка",
           cost: "2 500 Ꝑ/кг",
           pic: keta,
+          oldCost: null,
       },
       {
         id: 5,
         title: "Филе палтуса",
         cost: "2 800 Ꝑ/кг",
         pic: halibut,
+        oldCost: null,
     },
     ];
+
+    const cardCollection = [
+      {title: "С этим товаром покупают", productList},
+      {title: "Хиты продаж", productList},
+      {title: "Новинки", productList},
+      {title: "Рекомендуем", productList},
+    ]
     return (
         <div className="section">
-        <div className="section__container">
-              <div className="container__suggest  suggest">
-                <p className="suggest__title">С этим товаром покупают</p>
-                <ProductList productList={productList}></ProductList>
-        </div>
-        </div>
+          <div className="section__container">
+          {cardCollection.map(card => (  
+            <div className="container__suggest suggest">
+                <p className="suggest__title">{card.title}</p>
+                <ProductList productList={card.productList} />
+            </div>
+            ))}
+          </div>
         </div>
     );
 };
@@ -70,7 +84,12 @@ const ProductListItem = ({product}) => {
         <img width="341" height="256" src={product.pic}/>
         <p className="suggest-card-list__element--name">{product.title}</p>
         <div className="suggest-card-list__cart cart">
-          <p className="cart__cost">{product.cost}</p>
+        <p className="cart__cost">
+            {product.cost}
+            {product.oldCost && (
+                <span className="cart__cost--old">{product.oldCost}</span> 
+            )}
+          </p>
           <div className="cart_button">
             <a href="#" className="cart_button__cost--link">В корзину</a>
             <img width="17" height="17" class="cart_button__cost--img" src={cart}/>
